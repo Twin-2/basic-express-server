b95bda5cdfac89a2e67e731dfdee03b9bf4575bd
Twin-2 <davidwhit818@gmail.com>
2021-08-20 19:55:40 -0700

    finished lab 4

--- src/routes/games.js
+++ src/routes/games.js
@@ -2,64 +2,30 @@
 
 const express = require('express');
 const { Game } = require('../models/index.js');
-
+const Collection = require('../models/collection-class.js');
 const gameRoutes = express.Router();
 
+const Games = new Collection(Game);
 
-
-const addGame = async (req, res, next) => {
-    try {
-        let game = await Game.create(req.body)
-        res.status(201).json(game)
-    } catch (err) {
-        next(err)
-    }
-}
-
-const getAllGames = async (req, res, next) => {
-    try {
-        let allGames = await Game.findAll();
-        res.status(200).json(allGames)
-    } catch (err) {
-        next(err)
-    }
-}
-
-const getOneGame = async (req, res, next) => {
-    try {
-        let id = Number(req.params.id);
-        let game = await Game.findOne({ where: { id: id } })
-        res.status(200).json(game)
-    } catch (err) {
-        next(err)
-    }
-}
-
-const updateGame = async (req, res, next) => {
-    try {
-        let id = Number(req.params.id);
-        let game = await Game.findOne({ where: { id: id } })
-        let updatedGame = await game.update(req.body)
-        res.status(202).json(updatedGame);
-    } catch (err) {
-        next(err)
-    }
-}
-
-const deleteGame = async (req, res, next) => {
-    try {
-        let id = Number(req.params.id);
-        let deletedGame = await Game.destroy({ where: { id: id } })
-        res.send(204).send(deletedGame)
-    } catch (err) {
-        next(err)
-    }
-}
-
-gameRoutes.post('/games', addGame)
-gameRoutes.get('/games', getAllGames)
-gameRoutes.get('/games/:id', getOneGame)
-gameRoutes.put('/games/:id', updateGame)
-gameRoutes.delete('/games/:id', deleteGame)
+gameRoutes.post('/games', async (req, res, next) => {
+    let game = await Games.create(req.body)
+    res.status(201).send(game)
+})
+gameRoutes.get('/games', async (req, res, next) => {
+    let game = await Games.read()
+    res.status(200).send(game)
+})
+gameRoutes.get('/games/:id', async (req, res, next) => {
+    let game = await Games.read(req.params.id)
+    res.status(200).send(game)
+})
+gameRoutes.put('/games/:id', async (req, res, next) => {
+    let game = await Games.update(req.params.id, req.body)
+    res.status(202).send(game)
+})
+gameRoutes.delete('/games/:id', async (req, res, next) => {
+    let game = await Games.delete(req.params.id)
+    res.status(204).send(game)
+})
 
 module.exports = gameRoutes;
